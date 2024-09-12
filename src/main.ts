9bcbe9ae8f1df90bfb1136b7514d69cc860136e1
sidrashegaj <158463285+sidrashegaj@users.noreply.github.com>
2024-09-12 15:40:10 +0200

    nvabr/login/register

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,27 @@
+
+
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, Route } from '@angular/router';
+import { provideRouter, RouterModule, Route } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
-
 import { AppComponent } from './app/app.component';
+import { LoginComponent } from './app/components/login/login.component';
+import { RegisterComponent } from './app/components/register/register.component';
 import { CampgroundListComponent } from './app/components/campground-list/campground-list.component';
+import { NavbarComponent } from './app/components/navbar/navbar.component';
 
 const routes: Route[] = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'campgrounds', component: CampgroundListComponent },
   { path: '', redirectTo: '/campgrounds', pathMatch: 'full' },
+  { path: '**', redirectTo: '/campgrounds' } // Wildcard route for any undefined paths
 ];
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(), // Provides HttpClient
-  ],
+    provideHttpClient() // To enable HTTP requests in services
+  ]
 }).catch(err => console.error(err));
+
