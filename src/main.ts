a4b18bf914d8f12f0f09a2fae87421a10195681c
sidrashegaj <158463285+sidrashegaj@users.noreply.github.com>
2024-09-12 11:00:04 +0200

    fixed basic startup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideRouter, Route } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { CampgroundListComponent } from './app/components/campground-list/campground-list.component';
+
+const routes: Route[] = [
+  { path: 'campgrounds', component: CampgroundListComponent },
+  { path: '', redirectTo: '/campgrounds', pathMatch: 'full' },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(), // Provides HttpClient
+  ],
+}).catch(err => console.error(err));
